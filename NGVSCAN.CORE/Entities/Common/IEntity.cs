b93743b62ac33341e588c5548219099b5dfb6b11
Pavlo Grushetsky <pavlo.grushetsky@gmail.com>
2016-05-14 16:38:04 +0300

    Installed EF in DAL

---
+++ NGVSCAN.CORE/Entities/Common/IEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NGVSCAN.CORE.Entities.Common
+{
+    public interface IEntity
+    {
+        int Id { get; set; }
+
+        DateTime DateCreated { get; set; }
+
+        DateTime DateModified { get; set; }
+
+        bool IsDeleted { get; set; }
+
+        DateTime? DateDeleted { get; set; }
+    }
+}
