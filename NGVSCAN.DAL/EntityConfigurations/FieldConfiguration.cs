657559d6611c8e62fa44421d7cfb2bbc238cd136
Pavlo Grushetsky <pavlo.grushetsky@gmail.com>
2016-05-21 13:30:23 +0300

    Added FieldDetails control

--- NGVSCAN.DAL/EntityConfigurations/FieldConfiguration.cs
+++ NGVSCAN.DAL/EntityConfigurations/FieldConfiguration.cs
@@ -14,9 +14,8 @@ namespace NGVSCAN.DAL.EntityConfigurations
         public FieldConfiguration()
         {
             // Свойство Name установки - обязательно (не допускает значений NULL), 
-            // максимальная длина строки - 25 символов,
-            // свойство должно быть уникальным
-            Property(f => f.Name).IsRequired().HasMaxLength(25).HasUniqueIndex("IX_Field", 0);
+            // максимальная длина строки - 25 символов
+            Property(f => f.Name).IsRequired().HasMaxLength(25);
 
             // Свойство Description установки - обязательно (не допускает значений NULL), 
             // максимальная длина строки - 200 символов
---
+++ NGVSCAN.EXEC/Controls/FieldDetails.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NGVSCAN.CORE.Entities;
+
+namespace NGVSCAN.EXEC.Controls
+{
+    /// <summary>
+    /// Элемент управления для отображения детальной информации по вычислителю
+    /// </summary>
+    public partial class FieldDetails : UserControl
+    {
+        // Вычислитель
+        public Field Field { get; set; }
+
+        // Конструктор элемента управления
+        public FieldDetails()
+        {
+            InitializeComponent();
+        }
+
+        // Событие загрузки элемента управления
+        private void FieldDetails_Load(object sender, EventArgs e)
+        {
+            // Отображение названия и описания установки
+            labelField.Text = Floutec.Field.Name + " " + Floutec.Field.Description;
+
+            // Отображение адреса вычислителя
+            labelAddress.Text = Floutec.Address.ToString();
+
+            // Отображение названия вычислителя
+            labelName.Text = Floutec.Name;
+
+            // Отображение описания вычислителя
+            labelDescription.Text = Floutec.Description;
+
+            // Отображение даты и времени создания вычислителя
+            labelCreated.Text = Floutec.DateCreated.ToString("dd.MM.yyyy HH:mm:ss");
+
+            // Отображение даты и времени изменения вычислителя
+            labelModified.Text = Floutec.DateModified.ToString("dd.MM.yyyy HH:mm:ss");
+
+            // Инициализация поля отображения списка ниток вычислителя
+            labelLines.Text = "";
+
+            // Отображение списка ниток вычислителя
+            Floutec.MeasureLines.ToList().ForEach((l) => 
+            {
+                FloutecMeasureLine line = (FloutecMeasureLine)l;
+                labelLines.Text += line.Number + " " + line.Name + " ";
+            });
+        }
+    }
+}
