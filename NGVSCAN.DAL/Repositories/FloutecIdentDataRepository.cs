454f5e825528f245913c2ca8d9bff1225e44b24d
Pavlo Grushetsky <pavlo.grushetsky@gmail.com>
2016-05-26 00:31:59 +0300

    Started to implement dbf repositories

---
+++ NGVSCAN.DAL/Repositories/FloutecIdentDataRepository.cs
@@ -0,0 +1,48 @@
+﻿using NGVSCAN.CORE.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NGVSCAN.DAL.Repositories
+{
+    public class FloutecIdentDataRepository
+    {
+        #region Конструктор и поля
+
+        private readonly OleDbConnection _connection;
+
+        public FloutecIdentDataRepository(OleDbConnection connection)
+        {
+            _connection = connection;
+        }
+
+        #endregion
+
+        #region Вспомогательные методы
+
+        private static T GetReaderValue<T>(OleDbDataReader reader, string ordinal, T defaultValue = default(T))
+        {
+            try
+            {
+                return (T)Convert.ChangeType(reader[ordinal], typeof(T));
+            }
+            catch (Exception)
+            {
+                return defaultValue;
+            }
+        }
+
+        private static FloutecIdentData GetReaderValues(OleDbDataReader reader)
+        {
+            return new FloutecIdentData
+            {
+                
+            };
+        }
+
+        #endregion
+    }
+}
--- NGVSCAN.EXEC/MainForm.Designer.cs
+++ NGVSCAN.EXEC/MainForm.Designer.cs
@@ -29,19 +29,19 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("1 Газ на собственные нужды");
-            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("2 Газ на сепаратор С-1");
-            System.Windows.Forms.TreeNode treeNode10 = new System.Windows.Forms.TreeNode("185 Расход газа", new System.Windows.Forms.TreeNode[] {
-            treeNode8,
-            treeNode9});
-            System.Windows.Forms.TreeNode treeNode11 = new System.Windows.Forms.TreeNode("1 Конденсат. Налив в авто");
-            System.Windows.Forms.TreeNode treeNode12 = new System.Windows.Forms.TreeNode("2 Пластовая вода. Нали в авто");
-            System.Windows.Forms.TreeNode treeNode13 = new System.Windows.Forms.TreeNode("186 Расход жидкостей", new System.Windows.Forms.TreeNode[] {
-            treeNode11,
-            treeNode12});
-            System.Windows.Forms.TreeNode treeNode14 = new System.Windows.Forms.TreeNode("SEM-SRV", new System.Windows.Forms.TreeNode[] {
-            treeNode10,
-            treeNode13});
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("1 Газ на собственные нужды");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("2 Газ на сепаратор С-1");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("185 Расход газа", new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2});
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("1 Конденсат. Налив в авто");
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("2 Пластовая вода. Нали в авто");
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("186 Расход жидкостей", new System.Windows.Forms.TreeNode[] {
+            treeNode4,
+            treeNode5});
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("SEM-SRV", new System.Windows.Forms.TreeNode[] {
+            treeNode3,
+            treeNode6});
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.groupEstimatorsLog = new System.Windows.Forms.GroupBox();
             this.groupEstimatorsProperties = new System.Windows.Forms.GroupBox();
@@ -56,11 +56,11 @@
             this.menuDelete = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
             this.menuRestore = new System.Windows.Forms.ToolStripMenuItem();
+            this.imageList = new System.Windows.Forms.ImageList(this.components);
             this.status = new System.Windows.Forms.StatusStrip();
             this.menu = new System.Windows.Forms.MenuStrip();
             this.menuSettings = new System.Windows.Forms.ToolStripMenuItem();
             this.menuAbout = new System.Windows.Forms.ToolStripMenuItem();
-            this.imageList = new System.Windows.Forms.ImageList(this.components);
             this.groupEstimators.SuspendLayout();
             this.contextMenuEstimators.SuspendLayout();
             this.menu.SuspendLayout();
@@ -104,22 +104,22 @@
             this.treeEstimators.ImageList = this.imageList;
             this.treeEstimators.Location = new System.Drawing.Point(3, 16);
             this.treeEstimators.Name = "treeEstimators";
-            treeNode8.Name = "Node3";
-            treeNode8.Text = "1 Газ на собственные нужды";
-            treeNode9.Name = "Node4";
-            treeNode9.Text = "2 Газ на сепаратор С-1";
-            treeNode10.Name = "Node1";
-            treeNode10.Text = "185 Расход газа";
-            treeNode11.Name = "Node5";
-            treeNode11.Text = "1 Конденсат. Налив в авто";
-            treeNode12.Name = "Node6";
-            treeNode12.Text = "2 Пластовая вода. Нали в авто";
-            treeNode13.Name = "Node2";
-            treeNode13.Text = "186 Расход жидкостей";
-            treeNode14.Name = "Node0";
-            treeNode14.Text = "SEM-SRV";
+            treeNode1.Name = "Node3";
+            treeNode1.Text = "1 Газ на собственные нужды";
+            treeNode2.Name = "Node4";
+            treeNode2.Text = "2 Газ на сепаратор С-1";
+            treeNode3.Name = "Node1";
+            treeNode3.Text = "185 Расход газа";
+            treeNode4.Name = "Node5";
+            treeNode4.Text = "1 Конденсат. Налив в авто";
+            treeNode5.Name = "Node6";
+            treeNode5.Text = "2 Пластовая вода. Нали в авто";
+            treeNode6.Name = "Node2";
+            treeNode6.Text = "186 Расход жидкостей";
+            treeNode7.Name = "Node0";
+            treeNode7.Text = "SEM-SRV";
             this.treeEstimators.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
-            treeNode14});
+            treeNode7});
             this.treeEstimators.SelectedImageIndex = 0;
             this.treeEstimators.Size = new System.Drawing.Size(294, 351);
             this.treeEstimators.TabIndex = 0;
@@ -191,9 +191,19 @@
             this.menuRestore.Size = new System.Drawing.Size(202, 22);
             this.menuRestore.Text = "Восстановить";
             // 
+            // imageList
+            // 
+            this.imageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList.ImageStream")));
+            this.imageList.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList.Images.SetKeyName(0, "Factory-48.png");
+            this.imageList.Images.SetKeyName(1, "Module-48.png");
+            this.imageList.Images.SetKeyName(2, "DB 2-48.png");
+            this.imageList.Images.SetKeyName(3, "Ethernet Off-48.png");
+            this.imageList.Images.SetKeyName(4, "Electrical Threshold-48.png");
+            // 
             // status
             // 
-            this.status.Location = new System.Drawing.Point(0, 540);
+            this.status.Location = new System.Drawing.Point(0, 539);
             this.status.Name = "status";
             this.status.Size = new System.Drawing.Size(884, 22);
             this.status.SizingGrip = false;
@@ -225,21 +235,11 @@
             this.menuAbout.Size = new System.Drawing.Size(110, 20);
             this.menuAbout.Text = "О программе";
             // 
-            // imageList
-            // 
-            this.imageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList.ImageStream")));
-            this.imageList.TransparentColor = System.Drawing.Color.Transparent;
-            this.imageList.Images.SetKeyName(0, "Factory-48.png");
-            this.imageList.Images.SetKeyName(1, "Module-48.png");
-            this.imageList.Images.SetKeyName(2, "DB 2-48.png");
-            this.imageList.Images.SetKeyName(3, "Ethernet Off-48.png");
-            this.imageList.Images.SetKeyName(4, "Electrical Threshold-48.png");
-            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(884, 562);
+            this.ClientSize = new System.Drawing.Size(884, 561);
             this.Controls.Add(this.groupEstimatorsLog);
             this.Controls.Add(this.groupEstimatorsProperties);
             this.Controls.Add(this.status);
