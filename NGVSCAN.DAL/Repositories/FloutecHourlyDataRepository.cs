c81d0ac288affd609846e3b2418a30281c626c2e
kropl <kroplyk@gmail.com>
2016-05-31 20:47:47 +0300

    @

--- NGVSCAN.DAL/Repositories/FloutecHourlyDataRepository.cs
+++ NGVSCAN.DAL/Repositories/FloutecHourlyDataRepository.cs
@@ -3,6 +3,8 @@ using NGVSCAN.DAL.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Data.OleDb;
+using System.Diagnostics;
+using System.Linq;
 
 namespace NGVSCAN.DAL.Repositories
 {
@@ -22,28 +24,45 @@ namespace NGVSCAN.DAL.Repositories
         public List<FloutecHourlyData> Get(int address, int line, DateTime from, DateTime to)
         {
             List<FloutecHourlyData> hourlyData = new List<FloutecHourlyData>();
-            int n_flonit = address * 10 + line;
 
-            using (OleDbConnection connection = new OleDbConnection(_connectionString))
-            using (OleDbCommand command = connection.CreateCommand())
+            if (to >= from)
             {
-                connection.Open();
-                command.CommandText = "SELECT DISTINCT * FROM rour.DBF WHERE N_FLONIT=" + n_flonit + " AND CONVERT(DATETIME, DAT, 110) > ?";
-                command.Parameters.Add(new OleDbParameter("from", OleDbType.DBTimeStamp)).Value = from;
-                command.Parameters.Add(new OleDbParameter("to", OleDbType.DBTimeStamp)).Value = to;
+                int n_flonit = address * 10 + line;
 
-                using (OleDbDataReader reader = command.ExecuteReader())
+                List<DateTime> days = new List<DateTime>();
+
+                for (var date = from; date <= to; date = date.AddDays(1))
                 {
-                    while (reader.Read())
+                    days.Add(date);
+                }
+
+                Debug.WriteLine("Days list initialized");
+
+                using (OleDbConnection connection = new OleDbConnection(_connectionString))
+                using (OleDbCommand command = connection.CreateCommand())
+                {
+                    connection.Open();
+
+                    foreach (var date in days)
                     {
-                        hourlyData.FromHourTable(reader);
+                        command.CommandText = "SELECT DISTINCT * FROM rour.DBF WHERE N_FLONIT=" + n_flonit + " AND DAT LIKE('" + date.ToString("dd.MM.yyyy") + " __:00:00" + "')";
+
+                        using (OleDbDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                hourlyData.FromHourTable(reader);
+                            }
+                        }
+
+                        Debug.WriteLine("Data for day " + date.ToString("dd.MM.yyyy") + " readed");
                     }
                 }
+
+                hourlyData.ForEach(h => h.N_FLONIT = n_flonit);
             }
 
-            hourlyData.ForEach(h => h.N_FLONIT = n_flonit);
-
-            return hourlyData;
+            return hourlyData.Where(h => h.DAT >= from && h.DAT <= to).ToList();
         }
     }
 }
