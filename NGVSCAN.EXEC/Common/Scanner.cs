ceaa21fc4788aaee1829bc4763063e87113655d9
Pavlo Grushetsky <pavlo.grushetsky@gmail.com>
2016-06-16 14:48:55 +0300

    Deleted ROC809PeriodicDataType

--- NGVSCAN.EXEC/Common/Scanner.cs
+++ NGVSCAN.EXEC/Common/Scanner.cs
@@ -5,7 +5,6 @@ using NGVSCAN.DAL.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
-using System.Data.SqlClient;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
